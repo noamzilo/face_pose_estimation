0f50928864ad511f6b2206ac1eb8eb9d987827e9
Noam <noams@epd-medical.com>
2020-05-09 20:54:20 +0300

    cosmetics

--- pipeline/example.py
+++ pipeline/example.py
@@ -9,6 +9,7 @@ from src.acquisition.video_reader.VideoReader import VideoReader
 from src.post_processing.PostProcessor import PostProcessor
 
 
+
 def example_pipeline():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'Running on device: {device}')
@@ -20,29 +21,20 @@ def example_pipeline():
     video_reader = VideoReader(path_to_video=path_to_video, mode='PIL', downsample_factor=0.25)
     frames = video_reader.frames(start=60, end=120,)
 
-    # run video through MTCNN
     frames_tracked = []
-
     for i, frame in enumerate(frames):
         print(f'\rTracking frame: {i + 1}', end='')
 
-        # Detect faces
         bboxes, confidences = mtcnn.detect(frame)
+
         confidence_threshold = 0.95
-
         frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
+        frame = post_process_frame(bboxes, confidence_threshold, confidences, frame)
 
-        if bboxes is not None:
-            filtered_bboxes = [bbox for bbox, confidence in zip(bboxes, confidences) if confidence_threshold < confidence]
-            frame = PostProcessor.draw_rectengles(frame, filtered_bboxes)
-            frame = PostProcessor.blur_at_bboxes(frame, filtered_bboxes)
-
-        # display detections
         cv2.imshow(f'Frame', frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
-        # Add to frame list
         frames_tracked.append(frame)
 
     print(f'\nDone')
@@ -54,6 +46,14 @@ def example_pipeline():
     for frame in frames_tracked:
         video_tracked.write(frame)
     video_tracked.release()
+
+
+def post_process_frame(bboxes, confidence_threshold, confidences, frame):
+    if bboxes is not None:
+        filtered_bboxes = [bbox for bbox, confidence in zip(bboxes, confidences) if confidence_threshold < confidence]
+        frame = PostProcessor.draw_rectengles(frame, filtered_bboxes)
+        frame = PostProcessor.blur_at_bboxes(frame, filtered_bboxes)
+    return frame
 
 
 if __name__ == "__main__":
