a65c1abf52eb04be65171172c0ff1e28954a7872
Noam <noams@epd-medical.com>
2020-05-21 22:19:06 +0300

    using config file rather than hard coded

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -10,33 +10,41 @@ import cv2
 class Pipeline(object):
     def __init__(self):
         self._config = ConfigProvider.config()
+        self._device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self._mtcnn = MTCNN(keep_all=True, device=self._device)
+        self._video_reader = VideoReader(
+            path_to_video=self._config.data.path_to_video,
+            mode='PIL',
+            downsample_factor=0.25)
+
+        self._video_writer = self._create_video_writer()
+
+        self._start_frame, self._end_frame = 0, 200
+
+    def _create_video_writer(self):
+        video_writer_fourrcc = cv2.VideoWriter_fourcc(*'mp4v')
+        video_tracked = cv2.VideoWriter(
+            self._config.output.path_to_output_file,
+            video_writer_fourrcc,
+            self._video_reader.frame_rate,
+            self._video_reader.shape[:2])
+        return video_tracked
 
     def process_single_frame(self, frame):
         raise NotImplementedError
 
     def _example_pipeline(self):
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        print(f'Running on device: {device}')
-        # start_frame, end_frame = 60, 120
-        # start_frame, end_frame = 5, 10
-        start_frame, end_frame = 0, 200
+        print(f'Running on device: {self._device}')
 
-        mtcnn = MTCNN(keep_all=True, device=device)
-
-        # path_to_video = r"C:\noam\face_pose_estimation\data\videos\one_woman_occlusion.mp4"s
-
-        video_reader = VideoReader(path_to_video=self._config.data.path_to_video, mode='PIL', downsample_factor=0.25)
-        frames = video_reader.frames(start=start_frame, end=end_frame, )
+        frames = self._video_reader.frames(start=self._start_frame, end=self._end_frame, )
 
         frames_tracked = []
-        # bboxes_per_frame = []
-        # modify bboxes by temporal data
-        confidence_threshold = 0.95
+        confidence_threshold = self._config.confidence_threshold
         last_frame_filtered_bboxes, last_frame_confidences = None, None
         for i, frame in enumerate(frames):
             print(f'\rTracking frame: {i + 1}', end='')
 
-            bboxes, confidences = mtcnn.detect(frame)
+            bboxes, confidences = self._mtcnn.detect(frame)
 
             if bboxes is not None:
                 filtered_bboxes = [bbox for bbox, confidence in zip(bboxes, confidences) if
@@ -56,28 +64,14 @@ class Pipeline(object):
                 break
             frames_tracked.append(frame)
 
-        # post process by found bboxes
-        # frames = video_reader.frames(start=start_frame, end=end_frame)  # have to create a new generator for second pass
-        # for i, (frame, bboxes) in enumerate(zip(frames, bboxes_per_frame)):
-        #     frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
-        #     frame = post_process_frame(frame, bboxes)
-        #
-        #     cv2.imshow(f'Frame', frame)
-        #     if cv2.waitKey(25) & 0xFF == ord('q'):
-        #         break
-        #     frames_tracked.append(frame)
-
         print(f'\nDone')
 
-        # save tracked video
-        out_path = r'C:\noam\face_pose_estimation\output\output.mp4'
-        video_writer_fourrcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_tracked = cv2.VideoWriter(out_path, video_writer_fourrcc, video_reader.frame_rate, video_reader.shape[:2])
         for frame in frames_tracked:
-            video_tracked.write(frame)
-        video_tracked.release()
+            self._video_writer.write(frame)
+        self._video_writer.release()
 
-    def _post_process_frame(self, frame, bboxes):
+    @staticmethod
+    def _post_process_frame(frame, bboxes):
         if bboxes is not None:
             frame = PostProcessor.draw_rectengles(frame, bboxes)
             frame = PostProcessor.blur_at_bboxes(frame, bboxes)
