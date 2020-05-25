d51200959976e19ccf2325aa65e27002357d4368
Noam <noams@epd-medical.com>
2020-05-25 20:51:50 +0300

    UNTESTED tracking ids for objects.

--- statefull_processing/BboxTracker.py
+++ statefull_processing/BboxTracker.py
@@ -7,13 +7,15 @@ class BboxTracker(object):
     def __init__(self, ):
         self._config = ConfigProvider.config()
 
-        self._detections = {}
+        self._detections = np.array([])
+        self._ids = np.array([])
         self.__running_id = 0
 
     def update_tracked_bboxes(self, frame_index, new_bboxes):
-        remaining_det_inds, new_det_inds, lost_det_inds = self._track_with_same_place_iou(new_bboxes)
-        self._update_tracking(remaining_det_inds, new_det_inds, lost_det_inds)
-        return np.array(self._detections.values())
+        remaining_det_old_inds, remaining_det_new_inds, new_det_inds, lost_det_inds = \
+            self._track_with_same_place_iou(new_bboxes)
+        self._update_tracking(remaining_det_old_inds, remaining_det_new_inds, new_bboxes, new_det_inds, lost_det_inds)
+        return np.array(self._detections)
 
     def _track_with_same_place_iou(self, new_bboxes):
         """
@@ -27,12 +29,11 @@ class BboxTracker(object):
         lost_det_inds: np.array of inds in self._detections of detections that were found in previous frame but not in
                        the new frame
         """
-        old_dets = np.array(self._detections.values())
         if len(new_bboxes.shape) == 0:
-            return np.array(self._detections.values()), np.array([]), np.array(self._detections.values())
-        if len(old_dets.shape) == 0:
-            return np.array([]), np.arange(new_bboxes.shape[0]), np.array([])
-        ious = BboxUtils.ious(old_dets, new_bboxes)
+            return None
+        if len(self._detections.shape) == 0:
+            return None
+        ious = BboxUtils.ious(self._detections, new_bboxes)
         same_object_inds = np.where(self._config.tracking.iou_threshold < ious)
         same_object_inds_last_frame, same_object_inds_new_frame = same_object_inds
 
@@ -49,7 +50,22 @@ class BboxTracker(object):
 
         return remaining_det_old_inds, remaining_det_new_inds, new_det_inds, lost_det_inds
 
-    def _update_tracking(self, remaining_det_inds, new_det_inds, lost_det_inds):
-        old_detections = self._detections.values()  # counting on the dict maintaining order
-        add a translation: id to index. or index to id.
-        new_detections =
+    def _update_tracking(self, remaining_det_old_inds, remaining_det_new_inds, new_bboxes, new_det_inds, lost_det_inds):
+        old_all_detections = self._detections  # counting on the dict maintaining order
+        old_all_ids = self._ids
+
+        remaining_dets = new_bboxes[remaining_det_new_inds]
+        remaining_ids = old_all_ids[remaining_det_old_inds]
+
+        lost_det_ids = old_all_ids[lost_det_inds]
+
+        max_remaining_id = np.max(remaining_ids)
+
+        new_dets = new_bboxes[new_det_inds]
+        new_det_ids = np.arange(start=max_remaining_id + 1, stop=max_remaining_id + 1 + new_det_inds.shape[0])
+
+        new_all_dets = np.vstack(remaining_dets, new_dets)
+        new_all_ids = np.vstack(remaining_ids, new_det_ids)
+
+        self._detections = new_all_dets
+        self._ids = new_all_ids      
